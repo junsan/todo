504d9eeafbc75fbc5f2c2944f057945952a89bb1
Jun <xjunsantos@gmail.com>
2023-09-07 13:40:14 +0800

    Navbar and footer

---
+++ src/pages/TasksPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <main style="min-height: 50vh; margin-top: 2rem;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <!-- Add new Task -->
+
+                    <!-- List of uncompleted tasks -->
+                   
+
+                    <!-- show toggle button -->
+                    <div class="text-center my-3" >
+                        <button class="btn btn-sm btn-secondary" 
+                            >
+                            <span v-if="true">Show completed</span>
+                            <span v-else>Hide completed</span>
+                        </button>
+                    </div>
+
+                    <!-- list of completed tasks -->
+                    
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
